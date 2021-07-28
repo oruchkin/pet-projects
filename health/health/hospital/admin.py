1f64a0a0be9f04a03b9264d4fc48f8ce16156d5f
oruchkin <72818824+oruchkin@users.noreply.github.com>
2021-07-28 21:53:59 +0300

    hospital models created

--- health/health/hospital/admin.py
+++ health/health/hospital/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . models import Hospital_Type, Hospital
 
 # Register your models here.
+
+
+#admin.site.register(Hospital)
+admin.site.register(Hospital_Type)
+
+
+
+class HospitalAdmin(admin.ModelAdmin):
+    #this is slug key that takes name ask a value in tupple
+    #it fill in slug with converted name
+    prepopulated_fields={
+        "slug":('name',)
+    }
+
+admin.site.register(Hospital, HospitalAdmin)
--- health/health/hospital/models.py
+++ health/health/hospital/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Hospital_Type(models.Model):
+    type_name = models.CharField(max_length = 50)
+    slug = models.SlugField(max_length = 50)
+    
+    def __str__(self):
+        return self.type_name
+    
+
+
+class Hospital(models.Model):
+    hospital_id = models.IntegerField(primary_key=True)
+    category = models.ForeignKey(Hospital_Type, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length = 50)
+    address = models.TextField()
+    updated_date = models.DateTimeField(default=datetime.now)
+    # for image blank=True, null=True means it is not required
+    logo = models.ImageField(upload_to="images/hospital/", blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+
