1450aa1d96689eea610ef895983a41a1959bc0b4
oruchkin <72818824+oruchkin@users.noreply.github.com>
2021-07-29 22:13:03 +0300

    dispatcher finished

--- health/health/patient/urls.py
+++ health/health/patient/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
 
     path('allpatients/', views.getAllPatients, name="patients"),
+    #path('patient/<int:patientid>/', views.get_patient_info, name="single_patient"),
+    #path('patient/<str:slug>/', views.get_patient_info, name="single_patient"),
+    path('patient/<slug:name_slug>/', views.get_patient_info, name="single_patient"),
 ]
--- health/health/patient/views.py
+++ health/health/patient/views.py
@@ -9,5 +9,15 @@ def getAllPatients(request):
     context={
         "all_patients_key": all_patients,
     }
-
     return render(request, "patient/all_patient.html", context)
+
+
+def get_patient_info(request, name_slug):
+    get_one_patient = Patient.objects.filter(slug=name_slug) 
+    #patient is a table, inside it we have all objects, and we need special filter(where)
+    #SELECT * FROM patient WHERE patient_id=patientid
+
+    context = {
+        "get_one_patient_key": get_one_patient
+    }
+    return render(request, "patient/patient_details.html", context)
