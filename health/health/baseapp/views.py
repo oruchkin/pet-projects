7f0a1a2ed2f6d0dcc1a497cbe17aa80f655e6def
oruchkin <72818824+oruchkin@users.noreply.github.com>
2021-07-30 08:52:54 +0300

    model query start

--- health/health/baseapp/views.py
+++ health/health/baseapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from hospital.models import Hospital
+from patient.models import Patient
 
 # Create your views here.
 
 def index(request):
     return render(request,"baseapp/index.html")
 
+def all_model_quiries(request):
